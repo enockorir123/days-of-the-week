fc02d5edbd37186e37bdefc9cdf79c4bc58c1d9b
Enock Cheruiyot Korir <enockorir123@gmai.com>
2025-03-06 09:33:51 +0300

    switch case statement

--- main.js
+++ main.js
@@ -2,21 +2,30 @@
 const dayNumber = 10; 
 let day; // Using let since its value will change
 
-if (dayNumber === 1) {
-    console.log("Sunday");
-} else if (dayNumber === 2) {
-    console.log("Monday");
-} else if (dayNumber === 3) {
-    console.log("Teusday");
-} else if (dayNumber === 4) {
-    console.log("Wednesday");
-} else if (dayNumber === 5) {
-    console.log("Thursday");
-} else if (dayNumber === 6) {
-    console.log("Friday");
-} else if (dayNumber === 7) {
-    console.log("Saturday");
-} else {
+switch (dayNumber) {
+    case 1:
+        console.log("Sunday")
+    break;
+    case 2:
+        console.log("Monday")
+    break; 
+     case 3:
+    console.log("Teusday")
+break; 
+ case 4:
+console.log("Wednesday")
+break; 
+ case 5:
+console.log("Thursday")
+break; 
+ case 6:
+console.log("Friday")
+break; 
+ case 7:
+console.log("Saturday")
+break;
+default:
+ 
     console.log("Invalid input! Please enter a number between 1 and 7.");
 }
 
